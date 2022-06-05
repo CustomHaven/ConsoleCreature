ee7599f7dcf404a12e21831308d0859e095c6ddc
mmuse <mmuseaden@gmail.com>
2022-06-05 02:56:46 +0100

    Add project files.

---
+++ ConsoleCreature/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            // We will make something ASCII?? and we will learn how to print to the console
+            Console.WriteLine(" .-. ");
+            Console.WriteLine(" o o ");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Spiro the Specter");
+
+            Console.WriteLine("c_     ___");
+            Console.WriteLine("  \\\\__|__|__");
+            Console.WriteLine("　　 \\( o_o)");
+            Console.WriteLine("　　　 > ~  >");
+            Console.WriteLine("　　　/ 　 / \\\\");
+            Console.WriteLine("　　 /　　/　 \\\\");
+            Console.WriteLine("　　 \\　 )　　c/");
+            Console.WriteLine("　　/　 /");
+            Console.WriteLine("　 /　/|");
+            Console.WriteLine("　(　( \\ ");
+            Console.WriteLine("　|　|  \\ ");
+            Console.WriteLine("　| / \\ )");
+            Console.WriteLine("　| |　) |");
+            Console.WriteLine(" /  )(_/");
+            Console.WriteLine("(_ /");
+            Console.WriteLine("Dancing Man");
+
+        }
+    }
+}
